184cf223002e3135296aa831621a4733b93a165d
Bill0412 <jxphxufh@gmail.com>
2021-03-08 17:52:55 +0800

    fixed foobar with pyflakes

--- week7/foobar.py
+++ week7/foobar.py
@@ -1,11 +1,17 @@
 import time
 
-def foo():
-    return 42
+# def foo():
+#     return 42
 
 for foo in range(5):
     print(foo)
 bar = 1
 bar *= 0.2
 time.sleep(60)
-print(baz)
+print(bar)
+
+"""
+Fixed:
+foobar.py:6: redefinition of unused 'foo' from line 3
+foobar.py:11: undefined name 'baz'
+"""
